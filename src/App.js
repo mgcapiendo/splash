951539b1137b390156e4f8e8320b2aeeb56ea7fd
Manuel Capiendo <mgcapiendo2023@fau.edu>
2026-03-15 19:00:24 -0400

    added leaflet for map

--- src/App.js
+++ src/App.js
@@ -1,7 +1,25 @@
+import { MapContainer, TitleLayer, Marker, Popup, TileLayer } from "react-leaflet"
+
+const PHIL_FOSTER = { lat: 26.7845, lng: -80.0427 }
+
 function App() {
   return (
-    <div>
-      <h1>Splash</h1>
+    <div style={{height: '100vh', width: '100%' }}>
+      <MapContainer
+        center={[PHIL_FOSTER.lat, PHIL_FOSTER.lng]}
+        zoom={14}
+        style={{ height: '100%', width: '100%' }}
+      >
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="© OpenStreetMap contributors"
+        />
+
+        <Marker position={[PHIL_FOSTER.lat, PHIL_FOSTER.lng]}>
+          <Popup>Phil Foster Park</Popup>
+        </Marker>
+        
+      </MapContainer>
     </div>
   )
 }
